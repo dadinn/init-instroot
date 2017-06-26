a70333f3375ace8c5e7949b74a782fc0b5c596ca
Daniel Dinnyes <ddinnyes@gmail.com>
2017-06-26 16:47:35 +0100

    fixed partuuid fetching

--- init_instroot.sh
+++ init_instroot.sh
@@ -7,7 +7,7 @@ partuuid () {
 	exit 1
     fi
 
-    blkid -s PARTUUID -o value "$1$2"
+    sgdisk -i $2 $1 | grep -E '^Partition unique GUID:' | sed -e 's;^[^:]*: \([[:alnum:]-]*\)$;\1;' | tr '[:upper:]' '[:lower:]'
 }
 
 fsuuid () {
