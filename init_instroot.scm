f85faaeccca993738b2aa1906576d6a1b2a44022
Daniel Dinnyes <ddinnyes@gmail.com>
2018-08-21 14:26:15 +0100

    moved `install-deps-zfs` function to separate `common` submodule

--- init_instroot.scm
+++ init_instroot.scm
@@ -7,6 +7,7 @@
 
 (use-modules
  ((common utils) #:prefix utils:)
+ ((common zfs) #:prefix zfs:)
  ((ice-9 readline))
  ((ice-9 format))
  ((ice-9 regex) #:prefix regex:)
@@ -47,47 +48,6 @@
 	      (system "apt update")
 	      (system "apt install -y gdisk parted cryptsetup lvm2"))
 	    (error "Necessary binaries are missing" missing)))))
-
-(define (read-debian-version)
-  (let* ((rdr (open-input-file "/etc/debian_version"))
-	 (res (rdelim:read-string rdr))
-	 (res (regex:string-match "^[0-9]+" res))
-	 (res (regex:match:substring res))
-	 (res (string->number res)))
-    (close rdr)
-    res))
-
-(define (install-deps-zfs)
-  (when (not (file-exists? ".deps_zfs"))
-    (cond
-     ((file-exists? "/etc/debian_version")
-      (let ((release (read-debian-version)))
-	(case release
-	  ((8)
-	   (with-output-to-file "/etc/apt/sources.list.d/backports.list"
-	     (lambda ()
-	       (call-with-input-file "/etc/apt/sources.list"
-		 (lambda (port)
-		   (let* ((result (rdelim:read-string port))
-			  (pattern (make-regexp "^deb (.*) jessie main$" regexp/newline))
-			  (result (regex:match:substring (regexp-exec pattern result) 1)))
-		     (utils:println "deb" result "stretch" "main" "contrib"))))))
-	   (system* "apt" "update")
-	   (system* "apt" "install" "-y" "-t" "jessie-backports" "zfs-dkms"))
-	  ((9)
-	   (system* "sed" "-i" "-re" "s;^deb (.+) stretch main$;deb \\1 stretch main contrib;"
-		    "/etc/apt/sources.list.d/base.list")
-	   (system* "apt" "update")
-	   (system* "apt" "install" "-y" "zfs-dkms"))
-	  (else
-	   (error "Debian version is not supported" release)))))
-     (else
-      (error "Necessary binaries are missing, and unable to install them! Please make sure ZFS kernel modules are loaded and CLI commands are available (i.e. zpool and zfs)!")))
-    (when (not (zero? (system* "modprobe" "zfs")))
-      (error "ZFS kernel modules are missing!"))
-    (utils:println "ZFS kernel modules are loaded!")
-    (with-output-to-file ".deps_zfs"
-      (lambda () (display "")))))
 
 (define* (init-boot-parts boot-dev #:key uefi?)
     (cond
@@ -607,7 +567,7 @@ Valid options are:
       (cond
        (initdeps?
 	(install-deps-base)
-	(install-deps-zfs)
+	(zfs:install-deps-zfs)
 	(utils:println "Finished installing all package dependencies!"))
        (else
 	(when (not swap-size)
@@ -634,7 +594,7 @@ Valid options are:
 		 (zpool
 		  (when (and keyfile dev-list)
 		    (init-cryptdevs keyfile dev-list))
-		  (install-deps-zfs)
+		  (zfs:install-deps-zfs)
 		  (init-zfsroot zpool rootfs swap-size
 				#:swapfiles swapfiles
 				#:dir-list dir-list)
